321b1f7b6bbd709096b53ddd6172e438619c3013
Your Name <you@example.com>
2022-03-29 14:01:31 +1300

    Created Frontend tests for the login and the employees table

---
+++ cypress/integration/Auth.spec.js
@@ -0,0 +1,18 @@
+beforeEach(() => {
+    cy.visit('http://localhost:3000/login'); 
+});
+
+
+it('login with wrong username and password', () => {
+    cy.get('input[name="email"]').type('t3st@email.com'); // Find the input with the name "email", then type a value
+    cy.get('input[name="password"]').type('t3st'); // Find the input with the name "password", then type a value
+    cy.get('.loginButton').click(); // Find the element with the class .loginButton, then click it
+    cy.wait(2500)
+});
+
+it('login a user with email and password', () => {
+    cy.get('input[name="email"]').type('test@email.com'); // Find the input with the name "email", then type a value
+    cy.get('input[name="password"]').type('test'); // Find the input with the name "password", then type a value
+    cy.get('.loginButton').click(); // Find the element with the class .loginButton, then click it
+    cy.wait(2500)
+});
