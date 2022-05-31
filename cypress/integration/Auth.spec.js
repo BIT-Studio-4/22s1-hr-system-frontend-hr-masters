06911d59db067ce34ec88166db7830e80faf6cf2
DaneS25 <Raniscool124>
2022-06-01 11:43:28 +1200

    updated some cypress tests, WIP will continue at later date

--- cypress/integration/Auth.spec.js
+++ cypress/integration/Auth.spec.js
@@ -11,5 +11,5 @@ it('login a user with email and password', () => {
     cy.get('input[name="email"]').type('test@email.com'); // Find the input with the name "email", then type a value
     cy.get('input[name="password"]').type('test'); // Find the input with the name "password", then type a value
     cy.get('.loginButton').click(); // Find the element with the class .loginButton, then click it
-    cy.get('.table', {timeout: 10000}).contains('Sky');
+    cy.get('.mainContent' , {timeout: 10000}).contains('Employees');
 });
--- cypress/integration/Employee.spec.js
+++ cypress/integration/Employee.spec.js
@@ -9,6 +9,14 @@ it('login a user with email and password', () => {
     cy.get('.loginButton').click(); // Find the element with the class .loginButton, then click it
 });
 
+it('Fail to create person', () => {
+    cy.waitUntil(() => cy.get('.mainContent').contains('Employees'));
+    cy.get('[id^=create_button]').click();
+    cy.get('input[name="first_name"]').type('Kev');
+    cy.get('[id^=save_button]').click();
+    cy.get('.modal-body').contains('The given data was invalid.');
+    cy.get('[id^=cancel_button]').click();
+});
 
 it('Creating a new person', () => {
     cy.waitUntil(() => cy.get('.mainContent').contains('Employees'));
@@ -18,6 +26,7 @@ it('Creating a new person', () => {
     cy.get('input[name="username"]').type('BobbertTheBuilder');
     cy.get('input[name="email"]').type('bobbert@email.com');
     cy.get('[id^=save_button]').click();
+    cy.get('[id^=mainMessage]').contains('Update: 201 Created');
     cy.wait(1000);
     cy.reload();
 
@@ -29,13 +38,26 @@ it('Creating a new person', () => {
     cy.get('.table').contains('bobbert@email.com');   
 });
 
-it('Check employee update button', () => {
-    cy.get('[id^=update_Bobbert_Builder]').click(); // Find the id update_Builder_BobbertTheBuilder then click it 
+it('Check update employees cancel button', () => {
+    cy.get('[id^=update_Bobbert_Builder]').click();
     cy.get('[id^=cancel_button]').click();
 });
 
+it('Check employee update button', () => {
+    cy.get('[id^=update_Bobbert_Builder]').click(); // Find the id update_Builder_BobbertTheBuilder then click it 
+    cy.get('input[name="first_name"]').type('Bill');
+    cy.get('[id^=save_button]').click();
+    cy.get('[id^=mainMessage]').contains('Update: 200 OK');
+    cy.reload();
+});
+
+it('Check employee delete deny', () => {
+    cy.get('[id^=delete_Builder_BobbertTheBuilder]').click();
+    cy.get('[id^=delete_deny]').click();
+});
 
 it('Check employee delete button', () => {
     cy.get('[id^=delete_Builder_BobbertTheBuilder]').click();
     cy.get('[id^=delete_confirm]').click();
+    cy.get('[id^=mainMessage]').contains('Delete "Bill Builder": OK');
 });
